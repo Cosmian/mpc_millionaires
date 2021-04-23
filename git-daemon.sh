146643589eb66085c2fc51b81b836435f4e90b2a
Bruno Grieder <bruno.grieder@cosmian.com>
2021-04-23 07:52:39 +0200

    revised git deamon script

--- git-daemon.sh
+++ git-daemon.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# This scripts launches a git daemon which exposes this project at
-# `git://localhost:9418/mpc_millionaires`
+# This scripts launches a git daemon which exposes the project
 # 
 # It is useful to locally test modifications using the UI on the CipherCompute EAP version
 # 1. Make a change and test it using `./simulate.sh`
@@ -10,6 +9,12 @@
 # 4. Create/update a computation using the git URL above and the commit you want to test
 # 5. Run the computation fron the UI
 
-echo "git daemon starting on port 9418...ctrl+c to stop"
+echo "starting a git daemon...ctrl+c to stop"
+#get the iproute2 package 
+docker exec runtime_0 /bin/bash -c "apt update && apt install iproute2" &> /dev/null
+# grab host IP from comntainer
+hostip=$(docker exec runtime_0 /bin/bash -c "ip route" | awk '/default/ {print $3}')
+project=$(basename "$PWD")
+echo "git URL: git://${hostip}:9418/${project}"
 git daemon --reuseaddr --base-path=$(pwd)/.. --export-all --verbose $(pwd)/..
 echo "git daemon stopped"
